a8b4d874118f0c3da91ffb3f109f4b3fc7331980
Goodwish Matyila <goomatyila@gmail.com>
2019-05-20 03:18:02 +0200

    mobile adjust margins

--- production/company/welcome.js
+++ production/company/welcome.js
@@ -11,18 +11,18 @@ var templateOutput = `
         </div>
     </div>
 
-    <div class="row" style="color:white; text-shadow:1px 1px 1px black;opacity:.8;position:fixed; bottom:0px;left:0px;right:0px;background-color:black;padding:10em;">
-        <div class="col-md-4">
+    <div class="row" style="color:white; text-shadow:1px 1px 1px black;opacity:.8;position:fixed; bottom:0px;left:0px;right:0px;background-color:black;padding-bottom:10em;">
+        <div class="col-md-4 m-1">
             <h1>WORK</h1>
             <p>Use online workbook edit/export and save your excel documents on the go</p>
             <a href="#/spreadsheet" data-role="button" class="btn-block">Workbook Go</a>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 m-1">
         <h1>PLAN</h1>
         <p>Do you have a project submit a proporsal for development for your next big idea</p>
             <a href="#/timesheets" data-role="button" class="btn-block">Propose An Idea</a>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 m-1">
         <h1>SUPPORT</h1>
         <p>Get support from out team of experts ready to answer your questions 24x7x365</p>
             <a href="#/support" data-role="button" class="btn-block">Customer Support</a>
--- production/preloader.js
+++ production/preloader.js
@@ -1,6 +1,6 @@
 addEventListener('message', ()=>{
     postMessage(`
-        <div style="justify-content: center;text-align: center;padding:16em;">
+        <div style="justify-content: center;text-align: center;margin-top:16em;">
                     <img src="logo.png" class="loader"/>
                     <br/>
                     <h3 style="text-shadow:1px 1px 1px black;color:white; font-weight:100;">By Goodwish M</h3>
